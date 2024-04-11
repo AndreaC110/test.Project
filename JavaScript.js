b52e3775401f814ee2c7e94c93469644f1180552
AndreaC110 <acazaresvazquez@guilford.edu>
2024-04-11 14:39:00 -0400

    new

---
+++ JavaScript.js
@@ -0,0 +1,19 @@
+
+
+//
+
+myitem = document.getElementById("first");
+
+myitem.addEventListener("click", onClick);
+
+function onClick(onButtonClick) {  
+    myitem.style.color = "green";  
+}
+
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+thebutton.addEventListener("click", onButtonClick);
+
+function onButtonClick(color) {  
+    otheritem.style.color = "White";  
+}
